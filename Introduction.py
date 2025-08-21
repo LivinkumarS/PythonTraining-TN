c2f1c7eca40ad8ecd2ce04ecf4dbcc9805f8484b
LivinkumarS <001liv100@ggmail.com>
2025-08-21 12:54:18 +0530

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,35 @@
+# a=input("Enter a sentence: ") 
+
+# # this line of code is used to printout a.
+# # this line of code is used to printout a.
+# # this line of code is used to printout a.
+# # this line of code is used to printout a.
+# # this line of code is used to printout a.
+
+
+
+# print(a)
+
+# a="Hello world"
+# word="Python"
+
+# print(Word)
+
+# group1
+
+    # print("Step1")
+    # print("Step2")
+    # print("Step3")
+
+# group2
+
+    # print("Step4")
+    # print("Step5")
+    # print("Step6")
+
+# if False:
+#     print("Step1")
+#     print("Step2")
+    
+# print("Step3")
+# print("Final Step")
