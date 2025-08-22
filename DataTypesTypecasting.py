3a2d27b77f64926b1b876e3582030953347a18c9
LivinkumarS <001liv100@ggmail.com>
2025-08-22 12:46:34 +0530

    Datatypes and Type Casting

---
+++ DataTypesTypecasting.py
@@ -0,0 +1,11 @@
+name="Surya"
+role="Developer"
+age=22
+
+
+print("His name is Livinkumar. He is a Developer. His age is 22")
+print("His name is "+name+". He is a "+role+".")
+
+print("His name is " + name + ", his role is " + role + ", and his age is " + str(age)+".")
+
+print(f"His name is {name}, his role is {role}, and his age is {age}")
