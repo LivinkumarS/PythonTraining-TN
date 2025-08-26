3e4460a1029e7e22c4bfcc416207e2c39fc70a1d
LivinkumarS <001liv100@ggmail.com>
2025-08-26 12:57:15 +0530

    Control Flow

---
+++ ControlFlow.py
@@ -0,0 +1,16 @@
+# a=40
+# if a>5:
+#     if a>10:
+#         print("Very Large Number")
+#     else:
+#         print("Large Number")
+# else:
+#     print("Small number")
+    
+number=int(input("Enter Number: "))
+
+if  number & 1==0:
+    print("Even Number")
+else:
+    print("Odd Number")
+    
