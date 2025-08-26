56b135fbdf29c38e234213fbe19723d0e550b606
LivinkumarS <001liv100@ggmail.com>
2025-08-26 11:47:08 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,43 @@
+# # a=10
+# # b=20
+
+# # ans=a+b
+# # ans=a-b
+# # ans=a/b
+# # ans=a*b
+
+# # ans=40//3
+# # ans=2%3
+
+# # ans=4**0
+
+# # ans= -2+1*20/5 # -4(jubair), 2(sivaselvam, vishw, anitha), 
+
+# # print(ans)
+
+# # Comparison Operator.
+# a=10
+# b=15
+
+# # print(a==b)
+# # print(a!=b)
+# print(a>b)
+
+# # Logical Operators 
+
+# a=True
+# b=True
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+# Assignment Operator
+
+# a=10
+# # a+=1
+# # a-=1
+# # a*=1
+# # a/=1
+# # a%=1
+# a**=1
+# print(a)
