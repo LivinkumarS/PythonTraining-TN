58506301af73e6bedccfff0056cd615f1f0517a3
LivinkumarS <001liv100@ggmail.com>
2025-08-28 14:24:50 +0530

    Loops

---
+++ Loop.py
@@ -0,0 +1,38 @@
+# i=0 #-1,-2,-3,-4,-5
+
+# while i>10:
+#     print(i)
+#     i-=1
+# #
+
+# a="Hello Guys! You are here to learn Python"
+# print(a[-2])
+
+# for i in "Hello":
+#     print(i)
+#     print("Hi")
+
+
+# i=1
+# for num in range(3,15,2):
+#     print(num)
+
+# i=0
+# while i<10:
+#     if i==2:
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+    
+#i=0,1,2,3
+#execution count=1,2,3,4
+
+# Output: 
+# 0
+# 1
+
+for i in range(1,101):
+    if i%5==0:
+        break
+    print(i)
