a4c443e8023a049321ea875a62097ce3185c771f
LivinkumarS <001liv100@ggmail.com>
2025-09-02 11:21:32 +0530

    String

---
+++ String.py
@@ -0,0 +1,33 @@
+# b=a[1:10:2] #1,3,5,7,9
+# print(b)
+
+# print(a[5])
+
+# a[5]='z'
+# a="Hi World"
+
+
+
+# print(a*3)
+
+# print("or" not in a)
+
+
+# print(a.upper())
+# print(a.lower())
+
+# print(a.title())
+
+# print(a)
+# print(a.strip())
+
+# print(a.replace("hello","hi"))
+# print(a.split("."))
+# list1=["Hello ","World. ","I am ","Learning Python!"]
+# print("".join(list1))
+
+# a="hello.world.hi.guys"
+# print(a.find('van'))
+
+# print(a.startswith('Hel'))
+# print(a.endswith('s'))
